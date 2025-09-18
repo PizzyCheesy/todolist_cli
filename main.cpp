49f3ede245957a6df8c94d475c617ccda6a44ea2
Akhdan Zyhair <mrwolfeepizza@gmail.com>
2025-09-18 14:50:50 -0300

    added a feature: ls

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ using std::cin;
 std::vector<Task> tasks;
 Prompt prompt;
 
+void homePrompt();
+
 void printTask(Task& task) {
     cout << task.name << ": " << task.getStatus() << "\n";
 }
@@ -28,6 +30,7 @@ void createTask() {
     Task& newTask = tasks.back();  // reference the most recently created task
     
     printTask(newTask);
+    homePrompt();
 }
 
 void deleteTask() {
@@ -46,16 +49,29 @@ void deleteTask() {
     } else {
         std::cout << "Task not found!" << std::endl;
     }
+
+    homePrompt();
+}
+
+void printAllTasks() {
+    auto *pTasks = &tasks;
+
+    for(Task task : *pTasks) {
+        printTask(task);
+    }
+
+    homePrompt();
 }
 
 void homePrompt() {
     std::map<string, std::function<void()>> commands = {
         {"add", createTask},
-        {"delete", deleteTask}
+        {"delete", deleteTask},
+        {"ls", printAllTasks}
     };
 
     prompt.printText("===DASHBOARD===\n");
-    prompt.printText("1. Create new task: add\n2. Delete a task: delete\n>");
+    prompt.printText("1. Create new task: add\n2. Delete a task: delete\n3. List all tasks: ls\n>");
 
     string userInput = prompt.checkInput();
 
