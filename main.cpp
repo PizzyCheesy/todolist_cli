f7c63dfa4fc79027d276ac315bf088a7284c2e14
Akhdan Zyhair <mrwolfeepizza@gmail.com>
2025-09-18 13:49:19 -0300

    added a deleteTask() feature

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <optional>
 #include <map>
 #include <vector>
+#include <algorithm>
 
 #include "includes/task.h"
 #include "includes/prompt.h"
@@ -15,21 +16,37 @@ std::vector<Task> tasks;
 Prompt prompt;
 
 void printTask(Task& task) {
-    cout << task.name << ": " << task.getStatus();
+    cout << task.name << ": " << task.getStatus() << "\n";
 }
 
-void createTaskPrompt() {
-    string taskName = prompt.printText("Enter Task: ", true);
-
+void createTask(string taskName) {
     tasks.emplace_back(taskName, false);
     Task& newTask = tasks.back();  // reference the most recently created task
     
     printTask(newTask);
 }
 
-int main() {
-    createTaskPrompt();
+void deleteTask(const string& name) {
+    auto it = std::find_if(
+        tasks.begin(),
+        tasks.end(),
+        [&](const Task& t){ return t.name == name; }
+    );
+
+    if (it != tasks.end()) {
+        tasks.erase(it);
+        std::cout << "Task \"" << name << "\" deleted!" << std::endl;
+    } else {
+        std::cout << "Task not found!" << std::endl;
+    }
+}
+
+void homePrompt() {
     
+}
+
+int main() {
+
 
     return 0;
 }
